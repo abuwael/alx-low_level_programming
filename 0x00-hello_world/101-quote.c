62a9074bc8d6cc4556d94b086869c6bf90b800fc
abuwael <mahmoudwaeldev@gmail.com>
2023-09-07 11:48:12 -0700

    wada

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: 1
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error
+ * Return: Always  1.
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,59");
 	return (1);	
 }
