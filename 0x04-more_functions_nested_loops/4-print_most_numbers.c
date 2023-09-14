3fd543cf9b6722fee875d225bf970fbf51f5741a
abuwael <mahmoudwaeldev@gmail.com>
2023-09-14 04:42:33 -0700

    another answer in 4-print most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,15 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 48; num < 58; num++)
 	{
-		if (num == 2 && num == 4)
-			continue;
-		_putchar(num + 48);
+		if (num != 50)
+		{
+			if (num != 52)
+			{
+				_putchar(num);
+			}
+		}
 	}
 	_putchar('\n');
 }
